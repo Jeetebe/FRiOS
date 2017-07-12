418418c39f62608d62c1b22cfa7cf112d8fc6e00
MacBook <macbook@macbook-pro.local>
2017-07-12 20:21:59 +0700

    Initial Commit

---
+++ FRios/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FRios
+//
+//  Created by MacBook on 7/12/17.
+//  Copyright © 2017 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
